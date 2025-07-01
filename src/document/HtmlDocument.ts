8d099445855a9a00c9b79a6a9089ed7594be97e3
Benjamin Paquette <bpaquette@gmail.com>
2025-06-30 22:13:41 -0400

    Making the build method

--- src/document/HtmlDocument.ts
+++ src/document/HtmlDocument.ts
@@ -1,5 +1,15 @@
 import { BaseDocument } from "./baseDocument";
 
 export class HtmlDocument extends BaseDocument {
-    
+    build(): string {
+        return `<!DOCTYPE html>
+<html>
+<head>
+  <title>${this.name}</title>
+</head>
+<body>
+  <h1>${this.name}</h1>
+</body>
+</html>`;
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import * as fs from 'fs';
+import * as path from 'path';
 import { BaseDocument } from "./document/baseDocument";
 import { HtmlDocument } from "./document/HtmlDocument";
 
@@ -12,6 +14,18 @@ class Application {
 
     addHtmlDocument(document: HtmlDocument){
         this.htmlDocuments.push(document);
+    }
+
+    build(outputDir: string = 'dist'){
+        if (!fs.existsSync(outputDir)) {
+            fs.mkdirSync(outputDir, { recursive: true });
+        }
+
+        this.htmlDocuments.forEach(doc => {
+            const content = doc.build();
+            const filePath = path.join(outputDir, `${doc.name}.html`);
+            fs.writeFileSync(filePath, content);
+        });
     }
 }
 
