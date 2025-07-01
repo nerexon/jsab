28d7eef2d1d4da6e503d0232cfed494c6bda46fb
Benjamin Paquette <bpaquette@gmail.com>
2025-06-30 22:36:04 -0400

    Adding settitle method to html document

--- src/document/HtmlDocument.ts
+++ src/document/HtmlDocument.ts
@@ -1,11 +1,23 @@
 import { BaseDocument } from "./baseDocument";
 
 export class HtmlDocument extends BaseDocument {
+    public title: string;
+
+    constructor(name: string) {
+        super(name);
+        this.title = name;
+    }
+
+    setTitle(title: string){
+        this.title = title;
+        return this;
+    }
+
     build(): string {
         return `<!DOCTYPE html>
 <html>
 <head>
-  <title>${this.name}</title>
+  <title>${this.title}</title>
 </head>
 <body>
   <h1>${this.name}</h1>
