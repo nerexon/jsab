b0a57b09a26f8c31120020f27767e8e664fb5723
Benjamin Paquette <bpaquette@gmail.com>
2025-06-30 21:58:38 -0400

    Adding html documents

---
+++ src/document/HtmlDocument.ts
@@ -0,0 +1,5 @@
+import { BaseDocument } from "./baseDocument";
+
+export class HtmlDocument extends BaseDocument {
+    
+}
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import { BaseDocument } from "./document/baseDocument";
+import { HtmlDocument } from "./document/HtmlDocument";
 
 class Application {
     name: string
+    htmlDocuments: HtmlDocument[]
 
     constructor(name: string){
         this.name = name;
+        this.htmlDocuments = [];
     }
 
-    addHtmlDocument(){
-
+    addHtmlDocument(document: HtmlDocument){
+        this.htmlDocuments.push(document);
     }
 }
 
