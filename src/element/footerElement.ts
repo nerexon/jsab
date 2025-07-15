8d5d07b6af3e7b66f51c608ee3ecd9e7f5f44d13
Benjamin Paquette <bpaquette@proton.me>
2025-07-15 16:09:10 -0400

    Creating footerElement class

---
+++ src/element/footerElement.ts
@@ -0,0 +1,12 @@
+import { BaseContainerElement } from "./baseContainerElement";
+import { ElementType } from "./elementType";
+
+export class FooterElement extends BaseContainerElement {
+    constructor() {
+        super(ElementType.footer);
+    }
+
+    build() {
+        return `<footer>${this.childrens.map(child => (child as any).build()).join("\n")}</footer>`;
+    }
+}
