c741283323e459d7f6469fdfe2153b58d198788b
Benjamin Paquette <bpaquette@proton.me>
2025-07-15 17:05:55 -0400

    Making stuff readable

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import { Application } from "./application";
 import { BaseDocument } from "./document/baseDocument";
 import { HtmlDocument } from "./document/htmlDocument";
-import { HeaderElement } from './element/container/headerElement';
-import { MainElement } from './element/container/mainElement';
-import { FooterElement } from './element/container/footerElement';
+import { HeaderElement } from "./element/container/headerElement";
+import { MainElement } from "./element/container/mainElement";
+import { FooterElement } from "./element/container/footerElement";
 
-export { Application, BaseDocument, HtmlDocument, HeaderElement, MainElement, FooterElement };
+export {
+  Application,
+  BaseDocument,
+  HtmlDocument,
+  HeaderElement,
+  MainElement,
+  FooterElement,
+};
