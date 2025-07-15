4ede498bb01212c5d411a08c68b120d5c1170414
Benjamin Paquette <bpaquette@proton.me>
2025-07-15 16:17:15 -0400

    Cleaning the file

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,8 @@
-import * as fs from 'fs';
-import * as path from 'path';
+import { Application } from "./application";
 import { BaseDocument } from "./document/baseDocument";
 import { HtmlDocument } from "./document/htmlDocument";
+import { HeaderElement } from './element/headerElement';
+import { MainElement } from './element/mainElement';
+import { FooterElement } from './element/footerElement';
 
-class Application {
-    name: string
-    htmlDocuments: HtmlDocument[]
-
-    constructor(name: string){
-        this.name = name;
-        this.htmlDocuments = [];
-    }
-
-    addHtmlDocument(document: HtmlDocument){
-        this.htmlDocuments.push(document);
-        return this;
-    }
-
-    build(outputDir: string = 'dist'){
-        if (!fs.existsSync(outputDir)) {
-            fs.mkdirSync(outputDir, { recursive: true });
-        }
-
-        this.htmlDocuments.forEach(doc => {
-            const content = doc.build();
-            const filePath = path.join(outputDir, `${doc.name}.html`);
-            fs.writeFileSync(filePath, content);
-        });
-    }
-}
-
-export { Application, BaseDocument, HtmlDocument };
+export { Application, BaseDocument, HtmlDocument, HeaderElement, MainElement, FooterElement };
