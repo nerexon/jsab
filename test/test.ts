87c9b718f2a5b2da0c7eab2715ba2c6f67b3a113
Benjamin Paquette <bpaquette@proton.me>
2025-07-15 16:28:45 -0400

    Updating test file to use lastest update

--- test/test.ts
+++ test/test.ts
@@ -1,9 +1,31 @@
-import { Application, HtmlDocument } from '../src/index';
+import { Application, FooterElement, HeaderElement, HtmlDocument, MainElement } from '../src/index';
 
 const homePage = new HtmlDocument('Home')
     .setTitle("JSAB - Home");
 const aboutPage = new HtmlDocument('About')
     .setTitle("JSAB - About");
+
+homePage.body
+    .addElement(
+        new HeaderElement
+    )
+    .addElement(
+        new MainElement
+    )
+    .addElement(
+        new FooterElement
+    )
+
+aboutPage.body
+    .addElement(
+        new HeaderElement
+    )
+    .addElement(
+        new MainElement
+    )
+    .addElement(
+        new FooterElement
+    )
 
 const app = new Application('MyWebsite')
     .addHtmlDocument(homePage)
