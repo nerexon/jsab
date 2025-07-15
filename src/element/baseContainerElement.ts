11c8855a472f3c800eee1f5639b491e8bd2966e9
Benjamin Paquette <bpaquette@proton.me>
2025-07-15 15:36:14 -0400

    Improving security

--- src/element/baseContainerElement.ts
+++ src/element/baseContainerElement.ts
@@ -1,15 +1,20 @@
 import { BaseElement } from "./baseElement";
 import { ElementType } from "./elementType";
 
-export class BaseContainerElement extends BaseElement {
-    protected childrens: BaseElement[] = [];
+export abstract class BaseContainerElement extends BaseElement {
+    protected childrens: BaseElement[];
 
     constructor(type: ElementType){
         super(type);
+        this.childrens = new Array;
     }
 
-    addElement(element: BaseElement){
+    addElement(element: BaseElement): this{
         this.childrens.push(element);
         return this;
+    }
+
+    getElements(): ReadonlyArray<BaseElement> {
+        return this.childrens;
     }
 }
