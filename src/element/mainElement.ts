405283c23bf2c0af4bb6ea56f7bac476d58a296a
Benjamin Paquette <bpaquette@proton.me>
2025-07-15 16:07:04 -0400

    Creating MainElement class

---
+++ src/element/mainElement.ts
@@ -0,0 +1,12 @@
+import { BaseContainerElement } from "./baseContainerElement";
+import { ElementType } from "./elementType";
+
+export class MainElement extends BaseContainerElement {
+    constructor() {
+        super(ElementType.main);
+    }
+
+    build() {
+        return `<main>${this.childrens.map(child => (child as any).build()).join("\n")}`;
+    }
+}
