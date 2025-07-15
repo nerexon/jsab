a2841a0cbc80f598f37c4fb847a9d822d185f368
Benjamin Paquette <bpaquette@proton.me>
2025-07-15 15:23:01 -0400

    Reworking baseContainerElement

--- src/element/baseContainerElement.ts
+++ src/element/baseContainerElement.ts
@@ -2,14 +2,14 @@ import { BaseElement } from "./baseElement";
 import { ElementType } from "./elementType";
 
 export class BaseContainerElement extends BaseElement {
-    public children: BaseElement[] = [];
+    protected childrens: BaseElement[] = [];
 
     constructor(type: ElementType){
         super(type);
     }
 
     addElement(element: BaseElement){
-        this.children.push(element);
+        this.childrens.push(element);
         return this;
     }
 }
