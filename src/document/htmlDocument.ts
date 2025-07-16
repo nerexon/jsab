071c69839e880f9cb4f3ab105a23cce1cf5bcc20
Benjamin Paquette <bpaquette@proton.me>
2025-07-15 22:13:20 -0400

    FIX: Removing add element method

--- src/document/htmlDocument.ts
+++ src/document/htmlDocument.ts
@@ -7,13 +7,11 @@ import { TitleElement } from "../element/titleElement";
 export class HtmlDocument extends BaseDocument {
     public head: HeadElement;
     public body: BodyElement;
-    private elements: BaseHtmlElement[];
 
     constructor(name: string) {
         super(name);
         this.head = new HeadElement;
         this.body = new BodyElement;
-        this.elements = new Array;
     }
 
     setTitle(title: string){
@@ -24,17 +22,11 @@ export class HtmlDocument extends BaseDocument {
         return this;
     }
 
-    addElement(element: BaseHtmlElement){
-        this.elements.push(element)
-        return this;
-    }
-
     build(): string {
         return `<!DOCTYPE html>
 <html>
 ${this.head.build()}
 ${this.body.build()}
-${this.elements.map(element => (element as any).build()).join("\n")}
 </html>`;
     }
 }
