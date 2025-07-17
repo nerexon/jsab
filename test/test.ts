b3ac0b3ba0134d242ac90915ff3c89c7ad860a58
Benjamin Paquette <bpaquette@gmail.com>
2025-07-16 22:31:32 -0400

    TestPage: Using now div Element

--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,4 @@
-import { Application, FooterElement, H1Element, HeaderElement, HtmlDocument, MainElement, PElement } from '../src/index';
+import { Application, DivElement, FooterElement, H1Element, HeaderElement, HtmlDocument, MainElement, PElement } from '../src/index';
 
 const homePage = new HtmlDocument('Home')
     .setTitle("JSAB - Home");
@@ -16,13 +16,23 @@ homePage.body
     .addElement(
         new MainElement()
             .addElement(
-                new PElement()
-                .addText("This is a paragraph on the home page, demonstrating the use of PElement.")
-                .addText("It supports multiple text children and other formatting elements.")
+                new DivElement()
+                    .addElement(
+                        new PElement()
+                        .addText("This is a paragraph on the home page, demonstrating the use of PElement.")
+                        .addText("It supports multiple text children and other formatting elements.")
+                    )
             )
             .addElement(
-                new PElement()
-                .addText("You can add more content and structure here using other elements.")
+                new DivElement()
+                    .addElement(
+                        new PElement()
+                        .addText("You can add more content and structure here using other elements.")
+                    )
+                    .addElement(
+                        new PElement()
+                        .addText("Here is another paragraph to make the div element more noticeable.")
+                    )
             )
     )
     .addElement(
@@ -40,12 +50,22 @@ aboutPage.body
     .addElement(
         new MainElement()
             .addElement(
-                new PElement()
-                .addText("This project aims to simplify the process of building web applications with JavaScript.")
+                new DivElement()
+                    .addElement(
+                        new PElement()
+                        .addText("This project aims to simplify the process of building web applications with JavaScript.")
+                    )
             )
             .addElement(
-                new PElement()
-                .addText("More details about its features and development will be available soon.")
+                new DivElement()
+                    .addElement(
+                        new PElement()
+                        .addText("More details about its features and development will be available soon.")
+                    )
+                    .addElement(
+                        new PElement()
+                        .addText("This additional paragraph on the about page helps to illustrate the containerization by the div element.")
+                    )
             )
     )
     .addElement(
