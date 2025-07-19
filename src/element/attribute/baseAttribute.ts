7a05c4ffc23c5e11afee46caba266dfb3368e93c
Benjamin Paquette <bpaquette@gmail.com>
2025-07-19 15:00:26 -0400

    Creating the baseAttribute class

---
+++ src/element/attribute/baseAttribute.ts
@@ -0,0 +1,19 @@
+export abstract class BaseAttribute {
+    public name: Readonly<string>;
+    private _value: string;
+
+    constructor(name: string) {
+        this.name = name;
+        this._value = "";
+    }
+
+    protected setValue(value: string){
+        this._value = value;
+    }
+
+    public get value(){
+        return this._value;
+    }
+
+    public abstract build(): string;
+}
