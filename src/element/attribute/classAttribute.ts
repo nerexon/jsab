bb01abcab8dc4117edbf8259b789d43ff47b9ccd
Benjamin Paquette <bpaquette@gmail.com>
2025-07-22 15:14:04 -0400

    FIX: Returning this for chaining

--- src/element/attribute/classAttribute.ts
+++ src/element/attribute/classAttribute.ts
@@ -7,10 +7,12 @@ export class ClassAttribute extends BaseAttribute {
 
     public addClass(classname: string){
         this.setValue(this.value.split(" ").concat(classname).join(" "));
+        return this;
     }
 
     public addClasses(...classesName: (string | string[])[]){
         this.setValue(this.value.split(" ").concat(classesName.flatMap(className => typeof className === "string" ? className.split(" ") : className)).join(" "));
+        return this;
     }
 
     public get classes(): string[]{
